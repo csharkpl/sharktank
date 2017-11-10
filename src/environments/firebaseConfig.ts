076e5d16adcdc9ac00cecbe572c96535274a4e48
csharkpl <kamil.kwecka@cshark.pl>
2017-11-10 14:53:38 +0100

    Initial commit by ngx-firebase-bootstrap generator

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,11 @@
+
+
+export const firebaseConfig = {
+    // Paste all this from the Firebase console...
+    apiKey: "AIzaSyCedrwat-4z4H8VNiEzBnuqPWirgg7ALK8",
+    authDomain: "fishtank-af5d0.firebaseapp.com",
+    databaseURL: "https://fishtank-af5d0.firebaseio.com",
+    storageBucket: "fishtank-af5d0.appspot.com",
+    messagingSenderId: "1086029353222"
+};
+
